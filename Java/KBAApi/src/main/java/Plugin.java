49c1a8a9ff65e37b3d5893ce515e75373a1e8b55
FERNANDES <Dylan@yo.local>
2017-05-02 09:54:25 +0200

    Plugin Parser avec interface shell gestion plugin et premiere version de l’interface

---
+++ Java/KBAApi/src/main/java/Plugin.java
@@ -0,0 +1,23 @@
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface Plugin {
+
+	// id of the plugin
+	public String name();
+	
+	// plugin version
+	public int version();
+	
+	// min version
+	public int requiresCoreVersion();
+	
+	// max version suggested
+	public int testedWithCoreVersion();
+	
+}
---
+++ Java/KBACore/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            PluginParser.PluginParser();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+}
