e1cb6e326dafbc82dcdf21ba8423cf0f4b0fead6
GUITTON Candice <guitton.candice@gmail.com>
2017-07-21 00:20:57 +0200

    group manual

--- K-BA-API/controllers/group.js
+++ K-BA-API/controllers/group.js
@@ -37,6 +37,7 @@ GroupController.getByUid = function(uid) {
  * @returns {Promise<Group|undefined>}
  */
 GroupController.listGroups = function(user_id) {
+    /*
     var where = {};
     if(user_id) {
         where["$or"] = {
@@ -51,6 +52,8 @@ GroupController.listGroups = function(user_id) {
         }],
         where: where
     });
+    */
+    return ModelIndex.sequelize.query('SELECT `Group`.`id`, `Group`.`uid`, `Group`.`name`, `Group`.`created_at`, `Group`.`updated_at`, `Group`.`deleted_at`, `Group`.`owner_id`, `users`.`id` AS `users.id`, `users`.`uid` AS `users.uid`, `users`.`firstname` AS `users.firstname`, `users`.`lastname` AS `users.lastname`, `users`.`email` AS `users.email`, `users`.`pseudo` AS `users.pseudo`, `users`.`password` AS `users.password`, `users`.`birthdate` AS `users.birthdate`, `users`.`salt` AS `users.salt`, `users`.`created_at` AS `users.created_at`, `users`.`updated_at` AS `users.updated_at`, `users`.`deleted_at` AS `users.deleted_at`, `users`.`address_id` AS `users.address_id`, `users.GroupUser`.`created_at` AS `users.GroupUser.created_at`, `users.GroupUser`.`updated_at` AS `users.GroupUser.updated_at`, `users.GroupUser`.`group_id` AS `users.GroupUser.group_id`, `users.GroupUser`.`user_id` AS `users.GroupUser.user_id` FROM `Group` AS `Group` LEFT OUTER JOIN (`GroupUser` AS `users.GroupUser` INNER JOIN `User` AS `users` ON `users`.`id` = `users.GroupUser`.`user_id`) ON `Group`.`id` = `users.GroupUser`.`group_id` AND `users`.`deleted_at` IS NULL WHERE (`Group`.`deleted_at` IS NULL AND (`Group`.`owner_id` = ' + user_id +' OR `users.id` = ' + user_id + '))')
 };
 
 module.exports = GroupController;
