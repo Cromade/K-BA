2e5c27ef3975261b4429237a8787f7bac0b25001
GUITTON Candice <guitton.candice@gmail.com>
2017-07-22 17:18:26 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ K-BA-Android/app/src/main/java/projet/k_ba/item/ItemActivity.java
@@ -0,0 +1,15 @@
+package projet.k_ba.item;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import projet.k_ba.R;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+}
