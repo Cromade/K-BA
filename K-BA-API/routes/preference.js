48d6230141210f5dfbb5298c10056931903a9d0c
GUITTON Candice <guitton.candice@gmail.com>
2017-07-25 22:20:18 +0200

    pref

--- K-BA-API/routes/preference.js
+++ K-BA-API/routes/preference.js
@@ -36,12 +36,13 @@ router.get("/", (req, res, next) => {
                     itemCounter[item.uid].quantity++
                 })
             })
+            console.log(itemCounter);
             var resultItems = []
             var keys = Object.keys(itemCounter);
             keys.forEach((uid) => {
                 var item = itemCounter[uid]
                 if(item.quantity > 5) {
-                    resultItems.push(item);
+                    resultItems.push(item.item);
                 }
             })
             res.json(resultItems)
